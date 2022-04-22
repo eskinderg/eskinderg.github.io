d305c584114c62c37d0c8100f7f74d50b3b18d1e
Eskinder <eskinderget@gmail.com>
2022-04-22 16:55:23 +0300

    tootltip added to language button

--- src/app/components/langselect/langselect.module.ts
+++ src/app/components/langselect/langselect.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { LangSelectComponent } from './langselect';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   imports: [
     CommonModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTooltipModule
   ],
   declarations: [LangSelectComponent],
   exports: [LangSelectComponent]
