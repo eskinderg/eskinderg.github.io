a65606303b4ff7a8c09813b68e2b77748d6d6b68
Eskinder <eskinderget@gmail.com>
2022-04-22 19:40:19 +0300

    on push changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { LanguageService } from './providers/language.service';
 import { SpeedDialFabPosition } from './components/speeddial/speed-dial-fab.component';
 import { Meta } from '@angular/platform-browser';
@@ -6,8 +6,7 @@ import { Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
@@ -16,9 +15,9 @@ export class AppComponent {
   public selectedTheme: string;
   public selectedLang: string;
 
-  SpeedDialFabPosition          = SpeedDialFabPosition;
-  speedDialFabPosition          = SpeedDialFabPosition.Top;
-  speedDialFabColumnDirection   = 'column';
+  SpeedDialFabPosition = SpeedDialFabPosition;
+  speedDialFabPosition = SpeedDialFabPosition.Top;
+  speedDialFabColumnDirection = 'column';
   speedDialFabPositionClassName = 'speed-dial-container-top';
 
   public spieTags = [
@@ -37,12 +36,12 @@ export class AppComponent {
     this.selectedLang = localStorage.getItem('language');
 
     this.meta.addTags([
-      {name: 'og:title', content: 'Eskinder | Profile'},
-      {name: 'og:description', content: 'Hello, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. As confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.'},
-      {name: 'og:image', content: 'assets/img/social-min.png'},
-      {name: 'author', content: 'Eskinder'},
-      {name: 'keywords', content: 'Angular, Web, Frontend, Developer, Portfolio, Eskinder, Getahun'},
-      {name: 'description', content: 'Hello, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. I combine my knowledge and experience in order to develop professional web applications.'}
+      { name: 'og:title', content: 'Eskinder | Profile' },
+      { name: 'og:description', content: 'Hello, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. As confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.' },
+      { name: 'og:image', content: 'assets/img/social-min.png' },
+      { name: 'author', content: 'Eskinder' },
+      { name: 'keywords', content: 'Angular, Web, Frontend, Developer, Portfolio, Eskinder, Getahun' },
+      { name: 'description', content: 'Hello, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. I combine my knowledge and experience in order to develop professional web applications.' }
     ]);
 
     if (!this.languageService.texts) {
@@ -67,9 +66,9 @@ export class AppComponent {
     }
   }
 
-  onSpeedDialFabClicked(btn: {icon: string, theme: string}) {
+  onSpeedDialFabClicked(btn: { icon: string, theme: string }) {
     this.selectedTheme = btn.theme;
-    localStorage.setItem('theme',btn.theme);
+    localStorage.setItem('theme', btn.theme);
   }
 
   onSectionChange(sectionId: string) {
