cd930f9e86cba3e6a127ef4e81d0a1a09205d4fe
Eskinder <eskinderget@gmail.com>
2022-04-27 14:40:00 +0300

    google analytics changes

--- src/app/components/speeddial/speed-dial-fab.animations.ts
+++ src/app/components/speeddial/speed-dial-fab.animations.ts
@@ -15,7 +15,7 @@ export const speedDialFabAnimations = [
       transform: 'rotate(0deg)'
     })),
     state('active', style({
-      transform: 'rotate(225deg)'
+      transform: 'rotate(-90deg)'
     })),
     transition('* <=> *', animate('200ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
   ]),
