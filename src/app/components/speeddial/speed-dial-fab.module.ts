51fbf00dfc2919d1698c5d625bd1a415595b13dc
Eskinder <eskinderget@gmail.com>
2022-05-18 17:35:44 +0300

    color dropdown menu for smaller devices

--- src/app/components/speeddial/speed-dial-fab.module.ts
+++ src/app/components/speeddial/speed-dial-fab.module.ts
@@ -4,11 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { SpeedDialFabComponent } from './speed-dial-fab.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [SpeedDialFabComponent],
   imports: [
-    CommonModule, MatButtonModule, MatIconModule, MatTooltipModule
+    FlexLayoutModule, CommonModule, MatButtonModule, MatIconModule, MatTooltipModule
   ],
   exports: [SpeedDialFabComponent]
 })
