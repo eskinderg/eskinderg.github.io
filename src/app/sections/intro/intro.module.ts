c730e38dc2ea30778a59febb479b1c507a0f6a57
Eskinder <eskinderget@gmail.com>
2022-07-14 15:20:20 +0300

    tooltip for intro section

--- src/app/sections/intro/intro.module.ts
+++ src/app/sections/intro/intro.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IntroComponent } from './intro.component';
 import { SeparatorModule } from 'src/app/components/separator/separator.module';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [IntroComponent],
-  imports: [CommonModule, SeparatorModule],
+  imports: [
+    CommonModule,
+    MatTooltipModule,
+    SeparatorModule
+  ],
   exports: [IntroComponent]
 })
 export class IntroModule { }
