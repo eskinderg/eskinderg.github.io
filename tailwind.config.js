5b2b5f0673c3cea2a27439ba3431a3eed1b2e062
Eskinder <eskinderget@gmail.com>
2023-01-19 22:54:24 +0300

    fix alignments for few items

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,11 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
+      'xxs': '400px',
+      'xs': '640px',
       'sm': '768px',
-
       'md': '1024px',
-
       'lg': '1280px',
-
       'xl': '1536px',
     }
   },
