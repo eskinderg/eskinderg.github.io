ffa04f8478c4ea29a6c8899d0d8db19f9b740246
Eskinder <eskinderget@gmail.com>
2023-11-18 17:17:53 +0300

    meta tag add

--- src/app/app.meta.ts
+++ src/app/app.meta.ts
@@ -2,20 +2,21 @@ import { MetaDefinition } from "@angular/platform-browser";
 
 export const appMeta: MetaDefinition[] =
   [
-    { name     : 'og:title'       , content: 'Eskinder | Profile' },
-    { name     : 'og:description' , content: 'Hi, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. As confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.' },
-    { name     : 'og:image'       , content: 'assets/img/social-min.png' },
-    { name     : 'og:email'       , content: 'eskinderget@gmail.com' },
-    { name     : 'og:url'         , content: 'https://eskinderg.github.io' },
-    { name     : 'og:type'        , content: 'website' },
-    { name     : 'description'    , content: 'Hi, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. As confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.' },
-    { name     : 'reply-to'       , content: 'eskinderget@gmail.com' },
-    { name     : 'url'            , content: 'https://eskinderg.github.io' },
-    { name     : 'image'          , content: 'assets/img/social-min.png' },
-    { name     : 'author'         , content: 'Eskinder, eskinderget@gmail.com' },
-    { name     : 'keywords'       , content: 'Angular, Web, Frontend, Developer, Fullstack, Portfolio, Eskinder, Getahun' },
-    { name     : 'description'    , content: 'Hi, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. As confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.' },
-    { itemprop : 'name'           , content: 'Eskinder | Profile' },
-    { itemprop : 'description'    , content: 'Hi, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. As confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.' },
-    { itemprop : 'image'          , content: 'assets/img/social-min.png' }
+    { httpEquiv : 'Permissions-Policy' , content: 'interest-cohort=()'},
+    { name      : 'og:title'           , content: 'Eskinder | Profile' },
+    { name      : 'og:description'     , content: 'Hi, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. As confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.' },
+    { name      : 'og:image'           , content: 'assets/img/social-min.png' },
+    { name      : 'og:email'           , content: 'eskinderget@gmail.com' },
+    { name      : 'og:url'             , content: 'https://eskinderg.github.io' },
+    { name      : 'og:type'            , content: 'website' },
+    { name      : 'description'        , content: 'Hi, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. As confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.' },
+    { name      : 'reply-to'           , content: 'eskinderget@gmail.com' },
+    { name      : 'url'                , content: 'https://eskinderg.github.io' },
+    { name      : 'image'              , content: 'assets/img/social-min.png' },
+    { name      : 'author'             , content: 'Eskinder, eskinderget@gmail.com' },
+    { name      : 'keywords'           , content: 'Angular, Web, Frontend, Developer, Fullstack, Portfolio, Eskinder, Getahun' },
+    { name      : 'description'        , content: 'Hi, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. As confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.' },
+    { itemprop  : 'name'               , content: 'Eskinder | Profile' },
+    { itemprop  : 'description'        , content: 'Hi, my name is Eskinder Getahun. I am a Web Developer ( Web / FE - Angular ) and this is my portfolio page. As confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.' },
+    { itemprop  : 'image'              , content: 'assets/img/social-min.png' }
   ]
