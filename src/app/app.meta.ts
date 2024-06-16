4084a913ef954b486246dbb4f2e87aace07e9e90
Eskinder <eskinderget@gmail.com>
2024-06-16 19:03:37 +0300

    meta tags

--- src/app/app.meta.ts
+++ src/app/app.meta.ts
@@ -1,6 +1,12 @@
 import { MetaDefinition } from '@angular/platform-browser';
 
 export const appMeta: MetaDefinition[] = [
+  { name: 'title', content: 'Eskinder Getahun' },
+  {
+    name: 'description',
+    content:
+      'Hi, my name is Eskinder Getahun and I am a Web Developer. This is my portfolio page and as confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.'
+  },
   { httpEquiv: 'Permissions-Policy', content: 'interest-cohort=()' },
   { name: 'og:title', content: 'Eskinder Getahun' },
   {
@@ -8,7 +14,7 @@ export const appMeta: MetaDefinition[] = [
     content:
       'Hi, my name is Eskinder Getahun and I am a Web Developer. This is my portfolio page and as confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.'
   },
-  { name: 'og:image', content: 'assets/img/social-min.png' },
+  { name: 'og:image', content: 'https://eskinderg.github.io/assets/img/eskinder.webp' },
   { name: 'og:email', content: 'eskinderget@gmail.com' },
   { name: 'og:url', content: 'https://eskinderg.github.io' },
   { name: 'og:type', content: 'website' },
@@ -19,7 +25,7 @@ export const appMeta: MetaDefinition[] = [
   },
   { name: 'reply-to', content: 'eskinderget@gmail.com' },
   { name: 'url', content: 'https://eskinderg.github.io' },
-  { name: 'image', content: 'assets/img/social-min.png' },
+  { name: 'image', content: 'https://eskinderg.github.io/assets/img/eskinder.webp' },
   { name: 'author', content: 'Eskinder, eskinderget@gmail.com' },
   { name: 'keywords', content: 'Angular, Web, Frontend, Developer, Fullstack, Portfolio, Eskinder, Getahun' },
   {
@@ -33,5 +39,27 @@ export const appMeta: MetaDefinition[] = [
     content:
       'Hi, my name is Eskinder Getahun and I am a Web Developer. This is my portfolio page and as confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.'
   },
-  { itemprop: 'image', content: 'assets/img/social-min.png' }
+  { itemprop: 'image', content: 'https://eskinderg.github.io/assets/img/eskinder.webp' },
+  {
+    name: 'twitter:card',
+    content:
+      'Hi, my name is Eskinder Getahun and I am a Web Developer. This is my portfolio page and as confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.'
+  },
+  {
+    name: 'twitter:url',
+    content: 'https://eskinderg.github.io/'
+  },
+  {
+    name: 'twitter:title',
+    content: 'Eskinder Getahun'
+  },
+  {
+    name: 'twitter:description',
+    content:
+      'Hi, my name is Eskinder Getahun and I am a Web Developer. This is my portfolio page and as confirmed by my portfolio content and code, I combine my knowledge and experience in order to develop professional web applications.'
+  },
+  {
+    name: 'twitter:image',
+    content: 'https://eskinderg.github.io/assets/img/eskinder.webp'
+  }
 ];
