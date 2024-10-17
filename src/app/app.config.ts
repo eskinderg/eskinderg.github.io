2c15b1192a1f38577ff021f0b66ead7f3a0c1e51
Eskinder <eskinderget@gmail.com>
2024-10-17 14:44:07 +0300

    change app bootstrap

---
+++ src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ThemeModule } from './theme/theme.module';
+import { LanguageModule } from './language/language.modue';
+import { AppRoutingModule } from './app-routing.module';
+import { GoogleAnalyticsService } from './providers/google-analytics.service';
+import { AppInit } from './app.initializer';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { LoaderInterceptor } from './providers/loading.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        importProvidersFrom(BrowserModule, ThemeModule, LanguageModule, AppRoutingModule),
+        GoogleAnalyticsService,
+        AppInit,
+        provideHttpClient(withInterceptors([LoaderInterceptor])),
+        provideAnimations()
+    ]
+};
