9ea913e9ba809658f717f6ce8fe6762b17010625
Eskinder <eskinderget@gmail.com>
2026-08-07 19:31:54 +0300

    menu order change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
-import { ColorPickerComponent } from './components/main/color-picker/color-picker.component';
 import { RightComponent } from './components/main/right/right.component';
 import { LoaderComponent } from './components/main/loader/loader';
 import { OutlineComponent } from './components/main/outline/outline';
@@ -7,6 +6,7 @@ import { BaBackTopComponent } from './components/main/babacktop/babacktop.compon
 import { BaseComponent } from './sections/base.component';
 import { DynamicComponentsWrapperComponent } from './components/main/dynamic-components-wrapper.component';
 import { TrackerComponent } from './components/app/tracker/tracker.component';
+import { MenuComponent } from './components/main/menu/menu.component';
 
 @Component({
     selector: 'app-root',
@@ -14,7 +14,7 @@ import { TrackerComponent } from './components/app/tracker/tracker.component';
     styleUrls: ['./app.component.scss'],
     imports: [
         DynamicComponentsWrapperComponent,
-        ColorPickerComponent,
+        MenuComponent,
         RightComponent,
         LoaderComponent,
         OutlineComponent,
@@ -26,7 +26,7 @@ import { TrackerComponent } from './components/app/tracker/tracker.component';
 export class AppComponent extends BaseComponent {
     mainWrapper = viewChild.required<ElementRef>('mainWrapper');
 
-    colorPickerComponent = viewChild.required<ColorPickerComponent>(ColorPickerComponent);
+    // colorPickerComponent = viewChild.required<ColorPickerComponent>(ColorPickerComponent);
 
     @ViewChild(DynamicComponentsWrapperComponent)
     dynamicComponentsWrapper!: DynamicComponentsWrapperComponent;
--- src/app/components/main/right/right.component.ts
+++ src/app/components/main/right/right.component.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component, viewChild } from '@angular/core';
 import { ToggleComponent } from '../../app/toggle/toggle.component';
 import { LanguageDropDownComponent } from '../../app/langdropdown/langdropdown.component';
-import { MenuComponent } from '../menu/menu.component';
+import { ColorPickerComponent } from '../color-picker/color-picker.component';
 
 @Component({
     selector: 'app-right',
     templateUrl: './right.component.html',
     styleUrl: './right.component.scss',
-    imports: [ToggleComponent, MenuComponent, LanguageDropDownComponent],
+    imports: [ToggleComponent, ColorPickerComponent, LanguageDropDownComponent],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RightComponent {
