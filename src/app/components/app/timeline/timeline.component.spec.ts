f54bdb75eab4e7f71afd44b5ff44fc60c4b31337
Eskinder <eskinderget@gmail.com>
2026-09-09 18:54:32 +0300

    css animation change and test

--- src/app/components/app/timeline/timeline.component.spec.ts
+++ src/app/components/app/timeline/timeline.component.spec.ts
@@ -3,11 +3,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LanguageService } from '../../../providers/language.service';
 import { TimelineComponent } from './timeline.component';
 import { TitleComponent } from '../title/title.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LanguageServiceMock } from '../../../language/language.mock';
 import { ChipComponent } from '../chip/chip.component';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { beforeEach, describe, expect, it } from 'vitest';
+import lang from '../../../../assets/json/lang/en.json';
 
 describe('TimelineComponent', () => {
     let component: TimelineComponent;
@@ -15,7 +15,7 @@ describe('TimelineComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [BrowserAnimationsModule, TimelineComponent, TitleComponent, ChipComponent],
+            imports: [TimelineComponent, TitleComponent, ChipComponent],
             providers: [
                 {
                     provide: LanguageService,
@@ -30,30 +30,7 @@ describe('TimelineComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(TimelineComponent);
         component = fixture.componentInstance;
-        component.job = {
-            title: 'Freelance Web Developer (Full Stack)',
-            subtitle: 'Elance',
-            year: 'Sept 2018 - Present',
-            intro: 'Freelance',
-            elements: [
-                {
-                    type: 'list',
-                    header: 'Key responsibilities include, but not limited to:',
-                    list: [
-                        'Provided support for different websites ',
-                        'Created Single Page Applications by using web technologies like HTML5, SASS, Typescript, Angular2 and Bootstrap.',
-                        'Created and used Reducers that received Actions to modify the store State.',
-                        'Created custom NPM modules.',
-                        'Developed administrative UI Components using Angular2+, Typescript and Webpack',
-                        'Used Bootstrap for Responsive Web design.',
-                        'Optimized Front-End assets for speed and performance',
-                        'Used Middleware, Redux-Promise in an application to retrieve data from Back-End RESTFUL service.',
-                        'Created custom Angular Components including Breadcrumbs, Reactive Forms, Form Validators, Tabbed Components and Toggle Buttons.'
-                    ]
-                }
-            ],
-            used: ['Angular', 'Typescript', 'JQuery', 'Java Script', 'Bootstrap', 'Node JS', 'HTML5', 'Sass']
-        };
+        component.job = lang.experience.content;
         fixture.detectChanges();
     });
 
